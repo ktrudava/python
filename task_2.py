f7c726076134ba7d6fd2c32ef8b352fa49612eb3
Katsiaryna <44376447+ktrudava@users.noreply.github.com>
2021-01-22 19:17:24 +0300

    Delete task_2.py

--- task_2.py
+++
@@ -1,8 +0,0 @@
-lines_count = 0
-
-with open('2_file.txt') as f:
-    for line in f:
-        lines_count += 1
-        words_number = len(line.split())
-        print(f'Line # {lines_count} has {words_number} words.')
-    print(f'There are a total of {lines_count} lines in this file.')
