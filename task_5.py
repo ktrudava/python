be331d7a1be8b3ae1f70ce89a78bfc991769707b
Katsiaryna <44376447+ktrudava@users.noreply.github.com>
2021-01-22 19:17:51 +0300

    Delete task_5.py

--- task_5.py
+++
@@ -1,9 +0,0 @@
-f = open('5_file.txt', 'w')
-f.write(input('Please enter several numbers divided by space - '))
-f.close()
-
-with open('5_file.txt', 'r') as f:
-    content = f.readline()
-    line_list = content.split()
-
-print(f'The sum of the numberes entered is: {sum(int(i) for i in line_list)}.')
